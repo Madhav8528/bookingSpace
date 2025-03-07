086fc4da5cb5aefc88233cbc7ab4e3571a9cdb11
Madhav <madhavv8528@gmail.com>
2025-03-07 08:46:54 +0530

    appointment api managed

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -5,36 +5,71 @@ import { Appointment } from "../models/appointment.model.js";
 import { Doctor } from "../models/doctor.model.js";
 import paginate from "mongoose-paginate-v2";
 
+//a fuction to paginate result when required, can also use mongoose aggregate-paginate library
+function pagination(model) {
 
+    return async (req, res, next) => {
+
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page-1) * limit
+        const endIndex = page * limit
+
+        const result = {}
+
+        if(startIndex > 0){
+            result.previous = {
+                page :  page - 1,
+                limit : limit
+            }
+        }
+
+        if(endIndex < await model.countDocuments().exec()){
+            result.next = {
+                page :  page + 1,
+                limit : limit
+            }
+        }
+
+        try{
+
+            result.result = await model.find().limit(limit).skip(startIndex).exec()
+            res.pagination = result
+            next()
+        }
+        catch(err){
+            throw new ApiError(400, "Error during pagination", err)
+        }
+    }
+}
+
+
+//testing = Done(Success)
 //when user click on book an appointment and list of doctor is shown
+//can add filters also after
 const listDoctors = asyncHandler( async (req, res) => {
     
-    
-    // const profileData = {
-    //     name : doctors.fullName,
-    //     specialization : doctors.specialization,
-    //     picture : doctors.picture,
-    //     charges : doctors.charges
-    // }
-    // console.log(profileData);
-    const {page} = req.query.page
-    const {limit} = req.query.limit
-    const option = {
-        page : page,
-        limit : limit,
-    }
-    paginate(Doctor)
-    const doctors = Doctor.pa
-    if(!doctors){
-        throw new ApiError(400, "Something went wrong while fetching doctor list.")
-    }
-    console.log(doctors);
-    
+    //this will get list of user and paginate also as queried
+    //await pagination(Doctor)
+    //console.log(res.pagination);
     res.status(200)
-    .json( new apiResponse(200, Doctor, "Doctors data fetched successfully.") )
+    .json( new apiResponse(200, res.pagination, "Doctors data fetched successfully.") )
+})
 
+
+const breifProfileDoctor = asyncHandler( async (req, res) => {
+    
+})
+
+
+const bookAppointment = asyncHandler( async (req, res) => {
+    
+    
 })
 
 
 
-export { listDoctors }
+export { listDoctors,
+         pagination,
+         breifProfileDoctor }
--- src/routes/appointment.routes.js
+++ src/routes/appointment.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { listDoctors } from "../controllers/appointment.controller.js";
-
+import { listDoctors, pagination } from "../controllers/appointment.controller.js";
+import { Doctor } from "../models/doctor.model.js"
 
 const router = Router()
 
-router.route("/doctor-list").post(listDoctors)
+router.route("/doctor-list").post(pagination(Doctor),listDoctors)
 
 export default router;
